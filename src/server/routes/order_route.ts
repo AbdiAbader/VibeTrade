63282e884b058e37cb445d634f557a853b390331
AbdiAbader <105592398+AbdiAbader@users.noreply.github.com>
2023-06-21 15:25:53 +0300

    Delete src directory

--- src/server/routes/order_route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createOrder } from '../controller/order_controller';
-
-const OrderRouter = express.Router();
-
-OrderRouter.post('/', createOrder);
-
-export default OrderRouter;
